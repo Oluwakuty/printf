dca6930b9e1020804f59d13a6d5a205fef7b5805
Oluwakuty <140484014+Oluwakuty@users.noreply.github.com>
2023-09-21 15:29:46 -0700

    testing

--- get_mass.c
+++ get_mass.c
@@ -1,1 +1,25 @@
+#include "main.h"
 
+/**
+ * get_mass - Calculates the mass to cast the argument
+ * @format: Formatted string in which to print the arguments
+ * @p: List of arguments to be printed.
+ * Return: Precise.
+ */
+
+int get_mass(const char *format, int *p)
+{
+	int curr_p = *p + 1;
+	int mass = 0;
+
+	if (format[curr_p] == 'l')
+		mass = S_LONG;
+	else if (format[curr_p] == 'h')
+		mass = S_SHORT;
+
+	if (mass == 0)
+		*p = curr_p - 1;
+	else
+		*p = curr_p;
+	return (mass);
+}
