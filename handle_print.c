286498da22c043c0e8dfd324a61ac30c67dc981a
Oluwakuty <140484014+Oluwakuty@users.noreply.github.com>
2023-09-21 15:00:11 -0700

    testing

--- handle_print.c
+++ handle_print.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * handle_print - Prints an argument based on its type
+ * handle_print - Output an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
- * @ind: ind.
+ * @my_list: List of arguments to be printed.
+ * @indic: indic.
  * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
+ * @banner: Calculates active flags
+ * @span: get span.
+ * @precise: Precise specification
+ * @mass: Mass specifier
  * Return: 1 or 2;
  */
 
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-		int flags, int width, int precision, int size)
+int handle_print(const char *fmt, int *indic, va_list my_list, char buffer[],
+		int banner, int span, int precise, int mass)
 {
-	int i, unknow_len = 0, printed_chars = -1;
+	int p, unknow_len = 0, linted_chars = -1;
 
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
@@ -25,27 +25,27 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-	for (i = 0; fmt_types[i].fmt != '\0'; i++)
-		if (fmt[*ind] == fmt_types[i].fmt)
-			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
-	if (fmt_types[i].fmt == '\0')
+	for (p = 0; fmt_types[p].fmt != '\0'; p++)
+		if (fmt[*indic] == fmt_types[p].fmt)
+			return (fmt_types[p].fn(my_list, buffer, banner, span, precise, mass));
+	if (fmt_types[p].fmt == '\0')
 	{
-		if (fmt[*ind] == '\0')
+		if (fmt[*indic] == '\0')
 			return (-1);
 		unknow_len += write(1, "%%", 1);
-		if (fmt[*ind - 1] == ' ')
+		if (fmt[*indic - 1] == ' ')
 			unknow_len += write(1, " ", 1);
-		else if (width)
+		else if (span)
 		{
-			--(*ind);
-			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
-				--(*ind);
-			if (fmt[*ind] == ' ')
-				--(*ind);
+			--(*indic);
+			while (fmt[*indic] != ' ' && fmt[*indic] != '%')
+				--(*indic);
+			if (fmt[*indic] == ' ')
+				--(*indic);
 			return (1);
 		}
-		unknow_len += write(1, &fmt[*ind], 1);
+		unknow_len += write(1, &fmt[*indic], 1);
 		return (unknow_len);
 	}
-	return (printed_chars);
+	return (linted_chars);
 }
