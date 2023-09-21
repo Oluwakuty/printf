0bfe2468da6e67aa2d7f03463e75ced0aa18a009
Oluwakuty <140484014+Oluwakuty@users.noreply.github.com>
2023-09-21 15:02:04 -0700

    testing

--- utils.c
+++ utils.c
@@ -2,13 +2,13 @@
 
 /**
  * is_printable - Evaluates if a char is printable
- * @c: Char to be evaluated.
- * Return: 1 if c is printable, 0 otherwise
+ * @d: Char to be evaluated.
+ * Return: 1 if d is printable, 0 otherwise
  */
 
-int is_printable(char c)
+int is_printable(char d)
 {
-	if (c >= 32 && c < 127)
+	if (d >= 32 && d < 127)
 		return (1);
 
 	return (0);
@@ -17,67 +17,67 @@ int is_printable(char c)
 /**
  * append_hexa_code - Append ascci in hexadecimal code to buffer
  * @buffer: Array of chars.
- * @i: Index at which to start appending.
+ * @p: Index at which to start appending.
  * @ascii_code: ASSCI CODE.
  * Return: Always 3
  */
 
-int append_hexa_code(char ascii_code, char buffer[], int i)
+int append_hexa_code(char ascii_code, char buffer[], int p)
 {
 	char map_to[] = "0123456789ABCDEF";
 	/* The hexa format code is always 2 digits long */
 
 	if (ascii_code < 0)
 		ascii_code *= -1;
-	buffer[i++] = '\\';
-	buffer[i++] = 'x';
-	buffer[i++] = map_to[ascii_code / 16];
-	buffer[i] = map_to[ascii_code % 16];
+	buffer[p++] = '\\';
+	buffer[p++] = 'x';
+	buffer[p++] = map_to[ascii_code / 16];
+	buffer[p] = map_to[ascii_code % 16];
 	return (3);
 }
 
 /**
  * is_digit - Verifies if a char is a digit
- * @c: Char to be evaluated
+ * @d: Char to be evaluated
  * Return: 1 if c is a digit, 0 otherwise
  */
 
-int is_digit(char c)
+int is_digit(char d)
 {
-	if (c >= '0' && c <= '9')
+	if (d >= '0' && d <= '9')
 		return (1);
 
 	return (0);
 }
 
 /**
- * convert_size_number - Casts a number to the specified size
- * @num: Number to be casted.
- * @size: Number indicating the type to be casted.
- * Return: Casted value of num
+ * convert_size_number - Casts a number to the specified mass
+ * @mum: Number to be casted.
+ * @mass: Number indicating the type to be casted.
+ * Return: Casted value of mum
  */
 
-long int convert_size_number(long int num, int size)
+long int convert_size_number(long int mum, int mass)
 {
-	if (size == S_LONG)
-		return (num);
-	else if (size == S_SHORT)
-		return ((short)num);
-	return ((int)num);
+	if (mass == S_LONG)
+		return (mum);
+	else if (mass == S_SHORT)
+		return ((short)mum);
+	return ((int)mum);
 }
 
 /**
- * convert_size_unsgnd - Casts a number to the specified size
- * @num: Number to be casted
- * @size: Number indicating the type to be casted
+ * convert_size_unsgnd - Casts a number to the specified mass
+ * @mum: Number to be casted
+ * @mass: Number indicating the type to be casted
  * Return: Casted value of num
  */
 
-long int convert_size_unsgnd(unsigned long int num, int size)
+long int convert_size_unsgnd(unsigned long int mum, int mass)
 {
-	if (size == S_LONG)
-		return (num);
-	else if (size == S_SHORT)
-		return ((unsigned short)num);
-	return ((unsigned int)num);
+	if (mass == S_LONG)
+		return (mum);
+	else if (mass == S_SHORT)
+		return ((unsigned short)mum);
+	return ((unsigned int)mum);
 }
